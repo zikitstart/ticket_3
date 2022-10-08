81b2f23fd8974bfa47920fa639e729eae5d4bbc2
zikit <zikit.start@yandex.ru>
2022-10-08 17:44:40 +0300

    Сделал билет.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        String line = "158694276";
+        char [] lineToArray = line.toCharArray();
+        int l = lineToArray.length;
 
+        String line1 = line.substring(0,(l+1)/2);
+        String line2 = line.substring(line1.length(),l);
+
+        System.out.println(line1);
+        System.out.println(line2);
     }
 }
